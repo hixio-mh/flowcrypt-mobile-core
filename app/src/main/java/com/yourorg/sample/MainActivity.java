e292f75829e4195ec6c4e26ea57d1362181acd86
Tom J <tom@holub.me>
2018-11-29 17:31:40 +0000

    code style

--- app/src/main/java/com/yourorg/sample/MainActivity.java
+++ app/src/main/java/com/yourorg/sample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yourorg.sample;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,74 +12,76 @@ import java.io.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-        System.loadLibrary("node");
-    }
+  // Used to load the 'native-lib' library on application startup.
+  static {
+    System.loadLibrary("native-lib");
+    System.loadLibrary("node");
+  }
 
-    //We just want one instance of node running in the background.
-    public static boolean _startedNodeAlready=false;
+  //We just want one instance of node running in the background.
+  public static boolean _startedNodeAlready=false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        if( !_startedNodeAlready ) {
-            _startedNodeAlready=true;
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    startNodeWithArguments(new String[]{"node", "-e",
-                            "var http = require('http'); " +
-                                    "var versions_server = http.createServer( (request, response) => { " +
-                                    "  response.end('Versions: ' + JSON.stringify(process.versions)); " +
-                                    "}); " +
-                                    "versions_server.listen(3000);"
-                    });
-                }
-            }).start();
+    if( !_startedNodeAlready ) {
+      _startedNodeAlready=true;
+      new Thread(new Runnable() {
+        @Override
+        public void run() {
+          startNodeWithArguments(new String[]{"node", "-e",
+            "var http = require('http'); " +
+            "var versions_server = http.createServer( (request, response) => { " +
+            "  response.end('Versions2: ' + JSON.stringify(process.versions)); " +
+            "}); " +
+            "versions_server.listen(3000, 'localhost');"
+          });
         }
-
-        final Button buttonVersions = (Button) findViewById(R.id.btVersions);
-        final TextView textViewVersions = (TextView) findViewById(R.id.tvVersions);
-
-        buttonVersions.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                //Network operations should be done in the background.
-                new AsyncTask<Void,Void,String>() {
-                    @Override
-                    protected String doInBackground(Void... params) {
-                        String nodeResponse="";
-                        try {
-                            URL localNodeServer = new URL("http://localhost:3000/");
-                            BufferedReader in = new BufferedReader(
-                                    new InputStreamReader(localNodeServer.openStream()));
-                            String inputLine;
-                            while ((inputLine = in.readLine()) != null)
-                                nodeResponse=nodeResponse+inputLine;
-                            in.close();
-                        } catch (Exception ex) {
-                            nodeResponse=ex.toString();
-                        }
-                        return nodeResponse;
-                    }
-                    @Override
-                    protected void onPostExecute(String result) {
-                        textViewVersions.setText(result);
-                    }
-                }.execute();
-
-            }
-        });
-
+      }).start();
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native Integer startNodeWithArguments(String[] arguments);
+    final Button buttonVersions = (Button) findViewById(R.id.btVersions);
+    final TextView textViewVersions = (TextView) findViewById(R.id.tvVersions);
+
+    buttonVersions.setOnClickListener(new View.OnClickListener() {
+      @SuppressLint("StaticFieldLeak")
+      public void onClick(View v) {
+
+        //Network operations should be done in the background.
+        new AsyncTask<Void,Void,String>() {
+          @Override
+          protected String doInBackground(Void... params) {
+            StringBuilder nodeResponse = new StringBuilder();
+            try {
+              URL localNodeServer = new URL("http://localhost:3000/");
+              BufferedReader in = new BufferedReader(new InputStreamReader(localNodeServer.openStream()));
+              String inputLine;
+              while ((inputLine = in.readLine()) != null) {
+                nodeResponse.append(inputLine);
+              }
+              in.close();
+            } catch (Exception ex) {
+              nodeResponse = new StringBuilder(ex.toString());
+            }
+            return nodeResponse.toString();
+          }
+          @Override
+          protected void onPostExecute(String result) {
+            textViewVersions.setText(result);
+          }
+        }.execute();
+
+      }
+    });
+
+  }
+
+  /**
+   * A native method that is implemented by the 'native-lib' native library,
+   * which is packaged with this application.
+   */
+  @SuppressWarnings("JniMissingFunction")
+  public native Integer startNodeWithArguments(String[] arguments);
 }
