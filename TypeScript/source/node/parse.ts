aca0cfb26e2b00760c588ea0d13021c4a6e558a2
Tom J <tom@holub.me>
2018-12-21 13:34:39 +0000

    remove logging from node

--- TypeScript/source/node/parse.ts
+++ TypeScript/source/node/parse.ts
@@ -4,7 +4,6 @@
 
 import { IncomingMessage } from 'http';
 import { HttpClientErr, Buffers } from './fmt';
-import { Debug } from './endpoints';
 
 type ParseRes = { endpoint: string, data: Buffers, request: {} };
 const NEWLINE = Buffer.from('\n');
@@ -13,11 +12,11 @@ export const parseReq = (r: IncomingMessage): Promise<ParseRes> => new Promise((
   const initBuffers: Buffers = [];
   const data: Buffers = [];
   let newlinesEncountered = 0;
-  let totalLen = 0;
+  // let totalLen = 0;
   r.on('data', (chunk: Buffer) => {
-    totalLen += chunk.length;
-    console.log(`Received a chunk of data. Byte length: ${chunk.length}`);
-    Debug.printChunk('beginning of chunk in bytes', chunk);
+    // totalLen += chunk.length;
+    // console.log(`Received a chunk of data. Byte length: ${chunk.length}`);
+    // Debug.printChunk('beginning of chunk in bytes', chunk);
     let byteOffset = 0;
     while (newlinesEncountered < 2) {
       const nextNewlineIndex = chunk.indexOf(NEWLINE, byteOffset);
@@ -33,13 +32,13 @@ export const parseReq = (r: IncomingMessage): Promise<ParseRes> => new Promise((
     data.push(chunk.slice(byteOffset));
   });
   r.on('end', () => {
-    const initLen = initBuffers.map(b => b.length).reduce((a, b) => a + b);
-    const dataLen = data.map(b => b.length).reduce((a, b) => a + b);
-    console.log(`Reached end of stream. Total stream length: ${totalLen} of which ${initLen} was first two lines and ${dataLen} was data`);
-    Debug.printChunk('initBuffers in bytes', Buffer.concat(initBuffers));
-    Debug.printChunk('dataBuffers in bytes', Buffer.concat(data));
-    console.log('initBuffers', Buffer.concat(initBuffers).toString().split(''));
-    console.log('data', Buffer.concat(data).toString().split(''))
+    // const initLen = initBuffers.map(b => b.length).reduce((a, b) => a + b);
+    // const dataLen = data.map(b => b.length).reduce((a, b) => a + b);
+    // console.log(`Reached end of stream. Total stream length: ${totalLen} of which ${initLen} was first two lines and ${dataLen} was data`);
+    // Debug.printChunk('initBuffers in bytes', Buffer.concat(initBuffers));
+    // Debug.printChunk('dataBuffers in bytes', Buffer.concat(data));
+    // console.log('initBuffers', Buffer.concat(initBuffers).toString().split(''));
+    // console.log('data', Buffer.concat(data).toString().split(''))
     if (initBuffers.length && data.length) {
       try {
         const [endpointLine, requestLine] = Buffer.concat(initBuffers).toString().split(Buffer.from(NEWLINE).toString());
