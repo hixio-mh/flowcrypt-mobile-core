eda1c0399aa17ec70d98a1fbb0eb32f002f7a2c3
Tom J <tom@holub.me>
2018-12-28 11:46:14 +0000

    moving TS core types

--- TypeScript/source/platform/store.ts
+++ TypeScript/source/platform/store.ts
@@ -2,27 +2,7 @@
 
 'use strict';
 
-export type Contact = {
-  email: string; name: string | null; pubkey: string | null; has_pgp: 0 | 1; searchable: string[];
-  client: string | null; attested: boolean | null; fingerprint: string | null; longid: string | null; keywords: string | null;
-  pending_lookup: number; last_use: number | null;
-  date: number | null; /* todo - should be removed. email provider search seems to return this? */
-};
-
-export interface PrvKeyInfo {
-  private: string;
-  longid: string;
-  decrypted?: OpenPGP.key.Key;
-}
-
-export interface KeyInfo extends PrvKeyInfo {
-  public: string;
-  fingerprint: string;
-  primary: boolean;
-  keywords: string;
-}
-
-export type KeyInfosWithPassphrases = { keys: PrvKeyInfo[]; passphrases: string[]; };
+import { Contact } from '../core/pgp.js';
 
 export class Store {
 
