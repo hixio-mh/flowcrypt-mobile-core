c566e1979d9332093ea3270b22907647fae85dd2
Tom J <tom@holub.me>
2019-07-30 14:13:26 +0000

    code style

--- TypeScript/source/node/endpoints.ts
+++ TypeScript/source/node/endpoints.ts
@@ -24,7 +24,7 @@ export class Endpoints {
 
   public version = async (): Promise<Buffers> => {
     const hasNodeVersions = typeof process === 'object' && process && typeof process.versions === 'object' && process.versions && process.versions.openssl;
-    return fmtRes(hasNodeVersions ? process.versions : {app_version: VERSION});
+    return fmtRes(hasNodeVersions ? process.versions : { app_version: VERSION });
   }
 
   public encryptMsg = async (uncheckedReq: any, data: Buffers): Promise<Buffers> => {
