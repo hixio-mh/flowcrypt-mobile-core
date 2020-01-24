93b3f97e4e8144addefc33c5a052bb7513b51f43
Tom J <tom@holub.me>
2020-01-24 19:40:43 +0000

    fixed types ref

--- TypeScript/source/entrypoint-node.ts
+++ TypeScript/source/entrypoint-node.ts
@@ -1,6 +1,6 @@
 /* © 2016-2018 FlowCrypt Limited. Limitations apply. Contact human@flowcrypt.com */
 
-/// <reference path="../../../flowcrypt-node-modules/node_modules/@types/node/index.d.ts" />
+/// <reference path="../node_modules/@types/node/index.d.ts" />
 /// <reference path="./core/types/openpgp.d.ts" />
 
 'use strict';
