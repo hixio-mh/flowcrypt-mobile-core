95a75767a12c2342d36f610527e718a234c77598
Tom J <tom@holub.me>
2020-01-24 19:58:11 +0000

    updated licence year

--- source/entrypoint-node.ts
+++ source/entrypoint-node.ts
@@ -1,4 +1,4 @@
-/* © 2016-2018 FlowCrypt Limited. Limitations apply. Contact human@flowcrypt.com */
+/* © 2016-present FlowCrypt Limited. Limitations apply. Contact human@flowcrypt.com */
 
 /// <reference path="../node_modules/@types/node/index.d.ts" />
 /// <reference path="./core/types/openpgp.d.ts" />
